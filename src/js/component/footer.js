e1b2b4e8d3ee814398db65097174b2829fd8a334
113250674 <ariveratournoncr@gmail.com>
2021-03-05 02:32:39 +0000

    footer

---
+++ src/js/component/footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+export function footer() {
+return (
+<footer className="bg-light text-center text-lg-start">
+   <div className="text-center p-3" style="background-color: rgba(0, 0, 0, 0);">
+    © Copyright:
+    <a className="text-dark" href="#">Your Website 2019</a>
+  </div>
+ </footer>
+);
+};
